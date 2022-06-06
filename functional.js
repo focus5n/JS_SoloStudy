2f10e0c5357750b8bf2c1c3a9148b74e3fea12c6
hans <focus5n@naver.com>
2022-06-06 16:31:25 +0900

    제목 변경

--- functional.js
+++ functional.js
@@ -1,17 +1,18 @@
 // Data
 const users = [
   { id: 1, name: "ID", age: 36 },
-  { id: 2, name: "BJ", age: 32 },
-  { id: 3, name: "JM", age: 32 },
-  { id: 4, name: "PJ", age: 27 },
-  { id: 5, name: "HA", age: 25 },
-  { id: 6, name: "JE", age: 26 },
-  { id: 7, name: "JI", age: 31 },
-  { id: 8, name: "MP", age: 23 },
+  // { id: 2, name: "BJ", age: 32 },
+  // { id: 3, name: "JM", age: 32 },
+  // { id: 4, name: "PJ", age: 27 },
+  // { id: 5, name: "HA", age: 25 },
+  // { id: 6, name: "JE", age: 26 },
+  // { id: 7, name: "JI", age: 31 },
+  // { id: 8, name: "MP", age: 23 },
 ];
 
 const curriedFilter = _curryr(_filter);
 const curriedMap = _curryr(_map);
+const curriedEach = _curryr(_each);
 const _get = _curryr(function (object, key) {
   return object == null ? undefined : object[key];
 });
