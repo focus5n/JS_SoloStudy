f024759966158576fa673987ec53b14478368d24
hans <focus5n@naver.com>
2022-06-06 21:16:56 +0900

    그냥 자잘하게 수정

--- currying.js
+++ currying.js
@@ -1,19 +1,27 @@
 // 원본 정보
-const $users = [{ id: 1, name: "ID", age: 36 }]
+const $users = [{ id: 1, name: "ID", age: 36 },
+                // { id: 2, name: "BJ", age: 32 },
+                // { id: 3, name: "JM", age: 32 },
+                // { id: 4, name: "PJ", age: 27 },
+                // { id: 5, name: "HA", age: 25 },
+                // { id: 6, name: "JE", age: 26 },
+                // { id: 7, name: "JI", age: 31 },
+                // { id: 8, name: "MP", age: 23 },
+               ]
 // 조건만 입력하도록 매개변수의 순서를 반대로 변경함.
 const $curriedEach = _curryingReverse(_each)
 const $curriedFilter = _curryingReverse(_filter)
 const $curriedMap = _curryingReverse(_map)
 // 출력하길 원하는 key 값을 설정함. $get("keyName")
-const $get = _curryingReverse(function(object, key) {
+const $curriedGet = _curryingReverse(function(object, key) {
     return object == null ? undefined : object[key]
 })
 
-// 프로그램 시작지점입니다.
+// 프로그램 시작점임.
 // _go(계산하길 원하는 정보, 걸러내길 원하는 조건, 가공하길 원하는 조건, 로그 출력)
 _go($users,
     $curriedFilter(function(user) {return user.age >= 30}),
-    $curriedMap($get("name")),
+    $curriedMap($curriedGet("name")),
      console.log)
 
 // 데이터를 조건에 따라 걸러낸 결과값을 출력함.
@@ -44,7 +52,7 @@ function _reduce(conditions, executer, data) {
         conditions = _rest(conditions)
     }
 
-    _each(conditions,
+    $curriedEach(conditions,
           function(condition) {data = executer(data, condition)})
 
     return data
@@ -52,35 +60,35 @@ function _reduce(conditions, executer, data) {
 
 // 인자를 여럿 받는 함수 하나를, 인자를 하나씩 받는 함수 여럿으로 분리함.
 function _curryingReverse(calculator) {
-    return function(data, expression) {
-        return arguments.length == 2 ? calculator(data, expression) : function(expression) {
+    return function(data, executer) {
+        return arguments.length == 2 ? calculator(data, executer) : function(expression) {
             return calculator(expression, data)
         }
     }
 }
 
-// 계산하길 원하는 정보를 list 배열로 받고, 원하는 조건이 담긴 함수를 iterator 함수로 받아서, 조건 함수 개수만큼 반복문을 실행함.
+// 조건 함수를 실행하여 얻은 결과값을 반환함.
 function _each(conditions, executer) {
     const $keys = _key(conditions)
 
-    for(let index in $keys) {
+    for(const index in $keys) {
         executer(conditions[index], index)
     }
 
     return conditions
 }
 
-// 정보가 담긴 something 객체의 key 값을 배열에 담아서 반환하고, 객체가 아니라면 빈 배열로 치환하여 반환함.
+// 객체의 index 값을 반환함.
 function _key(something) {
-    return $isObject(something) == true ? Object.keys(something) : []
+    return _isObject(something) == true ? Object.keys(something) : []
 }
 
-// 정보가 담긴 something 인자가 객체인지 확인하고, 객체가 맞다면 true 값을 반환하고, 객체가 아니라면 false 값을 반환함.
-function $isObject(something) {
+// 객체인지 확인하여 boolean 값을 반환함.
+function _isObject(something) {
     return typeof something == "object" ? true : false 
 }
 
-// list 배열을 받아서 predicate 함수로 조건에 부합하는 정보만 걸러낸 뒤, 해당 정보만 담은 filteredList를 반환함.
+// 조건에 부합하는 값만 필터링한 배열을 반환함.
 function _filter(list, predicate) {
     const $filteredList = []
 
